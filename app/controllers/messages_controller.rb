7d2a86feb5d0fcf98214afcea226f2739d51cd56
mame <nakamura.sat@gmail.com>
2017-02-09 06:28:31 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,18 @@
 class MessagesController < ApplicationController
   def index
+    @aaa = Message.new
+    @messages=Message.all
+  end
+  
+  def create
+    @aaa=Message.new(message_params)
+    @aaa.save
+    redirect_to root_path , notice:"メッセージを保存しました"
+  # raise params.inspect
+  end
+  
+  private
+  def message_params
+    params.require(:aaa).permit(:name,:body)
   end
 end
