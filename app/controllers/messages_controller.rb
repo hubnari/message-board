1f7481efa3b5e1cd551e592d9758bb4c5a58705f
mame <nakamura.sat@gmail.com>
2017-02-10 15:49:22 +0000

    edit-delete

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
+  
+   before_action :set_message, only: [:edit, :update]
+  
   def index
     @message = Message.new
     @messages=Message.all
@@ -11,13 +14,40 @@ class MessagesController < ApplicationController
     else
       @messages=Message.all
       flash.now[:alert]="メッセージの保存に失敗しました"
+      #render action: :index
       render 'index'
     end
   # raise params.inspect
   end
   
+  def edit
+    
+  end
+  
+  def update
+    # @message=Message.find(params.id)
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: "メッセージを更新しました"
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render action: :index
+    end
+  end
+  
+  def destroy
+    @message=Message.find(params[:id])
+    @message.destroy
+   redirect_to root_path , notice: "メッセージを削除しました"
+  end
+  
   private
+  
   def message_params
     params.require(:message).permit(:name,:body)
+  end
+  
+  def set_message
+    @message=Message.find(params[:id])
   end
 end
