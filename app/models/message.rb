1025ac2f999c14afdaf9dfdf5b421558c354ae9f
mame <nakamura.sat@gmail.com>
2017-02-15 01:01:11 +0000

    デプロイの途中

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,4 @@
 class Message < ActiveRecord::Base
-<<<<<<< HEAD
   validates :name , length: {maximum: 20}, presence: true
   validates :body , length: {minimum: 2 , maximum: 20} , presence: true
-=======
-  validates :name , length: {maximum: 20}, presence: true
-  validates :body , length: {minimum: 2 , maximum: 20} , presence: true
->>>>>>> challenge1
 end
